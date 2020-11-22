88ac75832c3913245fd8035c057db3ee16afb5f5
will <will@zipcodes-MBP-8.fios-router.home>
2020-11-22 14:48:52 -0500

    classroom progress

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
 public class Classroom {
+    private Student[] students;
+
+    public Classroom(int maxNumberOfStudents){
+        students = new Student[maxNumberOfStudents];
+    }
+    public Classroom(Student[] student){
+        students = student;
+    }
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+        Double totalScore = 0.00;
+        for (int i = 0; i < students.length; i++){
+            totalScore += students[i].getAverageExamScore();
+        }
+        return totalScore / students.length;
+    }
+
+    //pick up with addStudent method after Exam 2
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,36 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void testClassroomConstructor(){
+        //Given
+        Classroom classroom = new Classroom();
+        Student[] expStudentArr = new Student[30];
+        //When
+        Student[] actStudentArr = classroom.getStudents();
+        //Then
+        Assert.assertEquals(expStudentArr, actStudentArr);
+    }
+
+    @Test
+    public void testGetAverageExamScore(){
+        //Given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {225.0, 25.0};
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+        Student[] students = {s1, s2};
+        Classroom classroom = new Classroom(students);
+        //When
+        double output = classroom.getAverageExamScore();
+        //Then
+        System.out.println(output);
+    }
+
+
+
 }
